c1a5f8fc9cdba057bfbe87554d9108422a21cec2
Srujal Shah <itechking007@gmail.com>
2024-01-28 02:06:06 +0530

    Change in content and footer added

--- components/universal/Footer.tsx
+++ components/universal/Footer.tsx
@@ -1,15 +1,13 @@
 "use client"
 
+import { Label } from "../ui/label"
+
 export default function Footer() {
-
-
     return (
         <footer className="flex justify-center m-10">
-            <div className="text-lg text-gray-400" style={{ fontFamily: "Borna" }}>       
+            <div className="text-lg text-gray-400" style={{ fontFamily: "Borna" }}>
+                <Label>© 2024 Apexia | प्रेमाने बनविलेले ❣️| All Rights Reserved</Label>
             </div>
         </footer>
     )
 }
-
-
-
